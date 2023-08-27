263e62b64ee2fd4e09215c3bc0902423453855e4
Hasnain-ansari <92669424+Hasnain-ansari@users.noreply.github.com>
2023-08-28 00:21:09 +0530

    Updated index1.js

--- index1.js
+++ index1.js
@@ -23,6 +23,7 @@ let convertCurrency = async () =>{
     let amount = document.querySelector("#amount").value;
     let fromCurrency = await fromDropDown.value;
     let toCurrency = await toDropDown.value;
+    const result = document.getElementById("result");
 
     if(amount.length != 0){
         fetch(api)
@@ -43,91 +44,3 @@ let convertCurrency = async () =>{
 document.querySelector("#convert-button").addEventListener("click", convertCurrency);
 window.addEventListener("load", convertCurrency);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////  
-//       differennt ways to fetch API
-
-
-    // let fetchRes = fetch(api)
-    // fetchRes.then(res => res.json()).then(data =>{
-    //     console.log(data);
-    //     for (const key in data.rates){
-    //         console.log(`${key} : ${data.rates[key]}`)
-    //     }
-    // })
-    
-    
-   
-    
-    // const data = res.json();
-    // for (const key in data.rates){
-    //     console.log(`${key} : ${data.rates[key]}`)
-    // }
-
-
-    // async function fetchAPI() {
-    //     const response = await fetch(api)
-    //     const data = await response.json();
-
-    //     console.log("mil gaya");
-    //     console.log(data)
-
-    //     for (const key in data.rates){
-    //         console.log(`${key} : ${data.rates[key]}`)
-    //     }
-        
-    // }
-
-    // fetchAPI()
-
-
-////////////////////////////////////////////////////////////////////other ways 
-
-    //  fetch(api)
-//     .then(res => res.json())
-//     .then(data => {console.log(data)
-//         for (const key in data.rates){
-//             console.log(`${key} : ${data.rates[key]}`)
-//         }
-//     });
-// let fetchRes = fetch(api)
-// fetchRes.then(res => res.json())
-//         .then(data => {console.log(data)
-//             for (const key in data.rates){
-//                 // console.log(`${key} : ${data.rates[key]}`)
-//                 const option1 = document.createElement("option");
-//                 const option2 = document.createElement("option");
-//                 option1.value = `${key}`;
-//                 option1.text = `${key}`;
-//                 option2.value = `${key}`;  
-//                 option2.text = `${key}`;
-//                 fromDropDown.add(option1);
-//                 toDropDown.add(option2);
-
-//                 //setting default value 
-//                 fromDropDown.value = "USD";
-//                 toDropDown.value = "INR";
-                
-//             }
-//         });
